b480892ed5fa29407fe4f634fb76e2f6cb9b302c
Céline Poinson <cpoinson@hotmail.fr>
2022-10-24 23:49:47 +0200

    add javascript conditions

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
+let youScore = 0;
+let computerScore = 0;
+const yodaBtn = document.querySelector("#yoda");
+const anakinBtn = document.querySelector("#anakin");
+const R2D2Btn = document.querySelector("#R2D2");
+const C3POBtn = document.querySelector("#C3PO");
+const BB8Btn = document.querySelector("#BB8");
+const chewbaccaBtn = document.querySelector("#chewbacca");
+const playerChoice = document.querySelectorAll("charactersBtn");
+
 const charactersBtn = document.querySelectorAll(".charactersBtn");
 const youChoice = document.querySelector("#youChoice");
+const allImages = document.querySelectorAll(".charactersImg");
+const imgArr = [...allImages].map((img) => {
+  return { src: img.src, alt: img.alt };
+});
 
-let imgArr = [
-  "./images/BB8.png",
-  "./images/C3PO.png",
-  "./images/chewbacca-blue.png",
-  "./images/chewbacca-green.png",
-  "./images/chewbacca-yellow.png",
-  "./images/dark-vador.png",
-  "./images/R2D2.png",
-  "./images/yoda-gold.png",
-  "./images/yoda-green.png",
-  "./images/yoda-yellow.png",
-  ".images/anakin.png",
-];
+const computerChoice = imgArr;
 
-// class characters {
-//   constructor(name) {
-//     this.name = name;
+const play = charactersBtn.forEach((button) => {
+  const img = button.querySelector("img");
+  const youChoiceImg = document.querySelector(".gameImg");
+  button.addEventListener("click", function () {
+    youChoiceImg.src = img.src;
+    youChoiceImg.alt = img.alt;
+
+    setTimeout(() => {
+      const computerChoiceImg = document.querySelector("#computer-img");
+
+      const randomImg = imgArr[Math.floor(Math.random() * imgArr.length)];
+      computerChoiceImg.src = randomImg.src;
+      computerChoiceImg.alt = randomImg.alt;
+      checkWinner(youChoiceImg, computerChoiceImg);
+    }, "1000");
+  });
+});
+
+// class Character {
+//   constructor() {
+//     this.character = character;
 //   }
 // }
 
-charactersBtn.forEach((button) => {
-  const img = button.querySelector("img");
-  const youChoiceImg = document.querySelector(".gameImg");
-  console.log(youChoiceImg);
-  button.addEventListener("click", function () {
-    youChoiceImg.src = img.src;
-  });
-});
+function checkWinner(player, computer) {
+  const playerSelection = player.alt;
+  const computerSelection = computer.alt;
+  console.log(playerSelection, computerSelection);
+  if (playerSelection === computerSelection) {
+    console.log(`It's a tie!`);
+  } else if (
+    playerSelection === "Yoda" &&
+    computerSelection === "Anakin Skywalker"
+  ) {
+    console.log("Computer wins!");
+  } else if (
+    playerSelection === "Anakin Skywalker" &&
+    computerSelection === "Yoda"
+  ) {
+    console.log("You win!");
+  } else if (playerSelection === "Yoda" && computerSelection === "R2D2") {
+    console.log("You win!");
+  } else if (playerSelection === "R2D2" && computerSelection === "Yoda") {
+    console.log("Computer wins!");
+  } else if (playerSelection === "Yoda" && computerSelection === "C3PO") {
+    console.log("Computer wins!");
+  } else if (playerSelection === "C3PO" && computerSelection === "Yoda") {
+    console.log("You win!");
+  }
+}
