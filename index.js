1965dbf9aef94800d54b6cab0e7f259386309f4c
Céline Poinson <cpoinson@hotmail.fr>
2022-10-24 09:45:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let imgArr = [
+  "./images/BB8.png",
+  "./images/C3PO.png",
+  "./images/chewbacca-blue.png",
+  "./images/chewbacca-green.png",
+  "./images/chewbacca-yellow.png",
+  "./images/dark-vador.png",
+  "./images/R2D2.png",
+  "./images/yoda-gold.png",
+  "./images/yoda-green.png",
+  "./images/yoda-yellow.png",
+  ".images/anakin.png",
+];
