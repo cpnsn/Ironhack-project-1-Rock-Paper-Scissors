e6f19ba5ec99e6af7bef4459256f2cd84f75df02
Céline Poinson <cpoinson@hotmail.fr>
2022-10-25 17:53:02 +0200

    js conditions, scores and start page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const R2D2Btn = document.querySelector("#R2D2");
 const C3POBtn = document.querySelector("#C3PO");
 const BB8Btn = document.querySelector("#BB8");
 const chewbaccaBtn = document.querySelector("#chewbacca");
-const playerChoice = document.querySelectorAll("charactersBtn");
+const startPage = document.querySelector("#startPage");
 
 const charactersBtn = document.querySelectorAll(".charactersBtn");
 const youChoice = document.querySelector("#youChoice");
@@ -17,16 +17,23 @@ const imgArr = [...allImages].map((img) => {
 
 const computerChoice = imgArr;
 
+const winner = document.querySelector("#winner");
+const pWinner = document.createElement("p");
+const textTie = document.createTextNode("IT'S A TIE!");
+const textYouWin = document.createTextNode("You win!");
+const textComputerWins = document.createTextNode("Computer wins!");
+const scoreYou = document.querySelector("#youResults");
+const scoreComputer = document.querySelector("#computerResults");
+
 const play = charactersBtn.forEach((button) => {
   const img = button.querySelector("img");
   const youChoiceImg = document.querySelector(".gameImg");
   button.addEventListener("click", function () {
     youChoiceImg.src = img.src;
     youChoiceImg.alt = img.alt;
-
+    const computerChoiceImg = document.querySelector("#computer-img");
+    computerChoiceImg.src = "./images/Question-mark.png";
     setTimeout(() => {
-      const computerChoiceImg = document.querySelector("#computer-img");
-
       const randomImg = imgArr[Math.floor(Math.random() * imgArr.length)];
       computerChoiceImg.src = randomImg.src;
       computerChoiceImg.alt = randomImg.alt;
@@ -35,105 +42,209 @@ const play = charactersBtn.forEach((button) => {
   });
 });
 
-// class Character {
-//   constructor() {
-//     this.character = character;
-//   }
-// }
-
 function checkWinner(player, computer) {
   const playerSelection = player.alt;
   const computerSelection = computer.alt;
   console.log(playerSelection, computerSelection);
+  winner.innerHTML = "";
   if (playerSelection === computerSelection) {
-    console.log(`It's a tie!`);
+    winner.append(textTie);
     //YODA
   } else if (
     playerSelection === "Yoda" &&
     computerSelection === "Anakin Skywalker"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "Anakin Skywalker" &&
     computerSelection === "Yoda"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (playerSelection === "Yoda" && computerSelection === "R2D2") {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (playerSelection === "R2D2" && computerSelection === "Yoda") {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (playerSelection === "Yoda" && computerSelection === "C3PO") {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (playerSelection === "C3PO" && computerSelection === "Yoda") {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (playerSelection === "Yoda" && computerSelection === "BB8") {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (playerSelection === "BB8" && computerSelection === "Yoda") {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (playerSelection === "Yoda" && computerSelection === "Chewbacca") {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (playerSelection === "Chewbacca" && computerSelection === "Yoda") {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "Yoda" &&
     computerSelection === "Darth Vader"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (
     playerSelection === "Darth Vader" &&
     computerSelection === "Yoda"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
     // ANAKIN
   } else if (
     playerSelection === "Anakin Skywalker" &&
     computerSelection === "R2D2"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "R2D2" &&
     computerSelection === "Anakin Skywalker"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (
     playerSelection === "Anakin Skywalker" &&
     computerSelection === "C3PO"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (
     playerSelection === "C3PO" &&
     computerSelection === "Anakin Skywalker"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "Anakin Skywalker" &&
     computerSelection === "BB8"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "BB8" &&
     computerSelection === "Anakin Skywalker"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (
     playerSelection === "Anakin Skywalker" &&
     computerSelection === "Chewbacca"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
   } else if (
     playerSelection === "Chewbacca" &&
     computerSelection === "Anakin Skywalker"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "Anakin Skywalker" &&
     computerSelection === "Darth Vader"
   ) {
-    console.log("Computer wins!");
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   } else if (
     playerSelection === "Darth Vader" &&
     computerSelection === "Anakin Skywalker"
   ) {
-    console.log("You win!");
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+    // R2D2
+  } else if (playerSelection === "R2D2" && computerSelection === "C3PO") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (playerSelection === "C3PO" && computerSelection === "R2D2") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (playerSelection === "R2D2" && computerSelection === "BB8") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (playerSelection === "BB8" && computerSelection === "R2D2") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (playerSelection === "R2D2" && computerSelection === "Chewbacca") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (playerSelection === "Chewbacca" && computerSelection === "R2D2") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (
+    playerSelection === "R2D2" &&
+    computerSelection === "Darth Vader"
+  ) {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (
+    playerSelection === "Darth Vader" &&
+    computerSelection === "R2D2"
+  ) {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+    // C3PO
+  } else if (playerSelection === "C3PO" && computerSelection === "BB8") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (playerSelection === "BB8" && computerSelection === "C3PO") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (playerSelection === "C3PO" && computerSelection === "Chewbacca") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (playerSelection === "Chewbacca" && computerSelection === "C3PO") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (
+    playerSelection === "C3PO" &&
+    computerSelection === "Darth Vader"
+  ) {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (
+    playerSelection === "Darth Vader" &&
+    computerSelection === "C3PO"
+  ) {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+    //BB8
+  } else if (playerSelection === "BB8" && computerSelection === "Chewbacca") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+  } else if (playerSelection === "Chewbacca" && computerSelection === "BB8") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (playerSelection === "BB8" && computerSelection === "Darth Vader") {
+    winner.appendChild(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (playerSelection === "Darth Vader" && computerSelection === "BB8") {
+    winner.append(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
+    // CHEWBACCA
+  } else if (
+    playerSelection === "Chewbacca" &&
+    computerSelection === "Darth Vader"
+  ) {
+    winner.append(textYouWin);
+    scoreYou.textContent = ++youScore;
+  } else if (
+    playerSelection === "Darth Vader" &&
+    computerSelection === "Chewbacca"
+  ) {
+    winner.appendChild(textComputerWins);
+    scoreComputer.textContent = ++computerScore;
   }
 }
+
+startPage.addEventListener("click", () => {
+  document.querySelector("#start-page").classList.add("hidden");
+  document.querySelector("#gamePage").classList.remove("hidden");
+});
